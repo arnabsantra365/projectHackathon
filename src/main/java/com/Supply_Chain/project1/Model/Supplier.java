ebc0085c941c7b9a64caaf473cd7f02b4595cc7f
uditpadhan98 <uditpadhan98@gmail.com>
2025-02-14 17:09:58 +0530

    order api updated

--- src/main/java/com/Supply_Chain/project1/Model/Supplier.java
+++ src/main/java/com/Supply_Chain/project1/Model/Supplier.java
@@ -19,6 +19,54 @@ public class Supplier {
     private Double rating;
     private LocalDateTime createdAt;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
     @OneToMany(mappedBy = "supplier", cascade = CascadeType.ALL)
     @JsonManagedReference
     private List<Product> products;
